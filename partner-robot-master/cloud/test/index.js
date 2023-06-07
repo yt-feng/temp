98e4061c518a34c9cfb3ac9f6413736884dc3379
Yutong <yt.feng@foxmail.com>
2023-06-08 03:22:53 +0800

    delete

--- partner-robot-master/cloud/test/index.js
+++
@@ -1,9 +0,0 @@
-// 云函数入口文件
-const cloud = require('wx-server-sdk')
-cloud.init()
-const db = cloud.database()
-cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
-// 云函数入口函数
-exports.main = async (event, context) => {
-  db.collection("agent").where({code:"c4ca4238a0b"}).update({data:{fee:0}})
-}
